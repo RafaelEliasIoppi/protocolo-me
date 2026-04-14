d4d706bba812330ab2072757a66d4ec26eae37f5
rafaelioppi <141973185+rafaelioppi@users.noreply.github.com>
2026-04-14 12:27:36 +0000

    rafael

--- backend/src/main/java/back/controller/UsuarioController.java
+++ backend/src/main/java/back/controller/UsuarioController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import transportadora.model.Usuario;
-import transportadora.service.UsuarioService;
+import back.model.Usuario;
+import back.service.UsuarioService;
 
 
 @RestController
