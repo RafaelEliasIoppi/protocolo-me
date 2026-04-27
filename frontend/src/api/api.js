975a1f335859f777f5032eeda3d2bf382ac16bab
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2026-04-27 13:49:32 +0000

    rafael

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.REACT_APP_API_URL ||
-  (process.env.NODE_ENV === "development" ? "/" : "http://localhost:2500");
+  process.env.REACT_APP_API_URL || `${window.location.origin}/api`;
 
 const api = axios.create({
   baseURL,
